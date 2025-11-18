a65d143c1971d7a9e11c855bb28a1b8d29cdb255
Juan Uribe <uribejuanb@javeriana.edu.co>
2025-11-18 18:22:23 -0500

    Primer commit iniciación repo

---
+++ parcialback/src/main/java/com/example/parcialback/ParcialbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.parcialback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialbackApplication.class, args);
+	}
+
+}
---
+++ parcialback/src/test/java/com/example/parcialback/ParcialbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parcialback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
