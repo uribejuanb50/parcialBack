87b614406386cbb9793bfc65a519385be589f31c
Juan Uribe <uribejuanb@javeriana.edu.co>
2025-11-18 19:45:50 -0500

    Añadida capa servicios

--- parcialback/src/main/java/com/dtos/ClinicaRegisterDTO.java
+++ parcialback/src/main/java/com/dtos/ClinicaRegisterDTO.java
@@ -1,5 +1,12 @@
 package com.dtos;
 
-public class ClinicaRegisterDTO {
+import lombok.Data;
 
+@Data
+public class ClinicaRegisterDTO{
+    String nombreClinica;
+    String direccionClinica;
+    int nCamas;
+    Integer numeroClinica;
+    String ciudadClinica;
 }
---
+++ parcialback/src/main/java/com/dtos/MedicoRegisterDTO.java
@@ -0,0 +1,5 @@
+package com.dtos;
+
+public class MedicoRegisterDTO {
+
+}
